266112adf114e6fe9b55eff7ec1e10e0f6e8b611
hajar zerhouni <hajar.zerhounipr@gmail.com>
2025-07-26 20:41:09 +0100

    logement admin dashboard

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import {HomeComponent} from './pages/home/home.component';
 import {LoginComponent} from './auth/login/login.component';
 import {RegisterComponent} from './auth/register/register.component';
+import {AdminComponent} from './pages/adminDashboard/admin/admin.component';
 
 export const routes: Routes = [
   {path: '' , redirectTo: 'home' , pathMatch: 'full'},
   {path : 'home' , component: HomeComponent},
   {path : 'login' , component: LoginComponent},
-  {path : 'register' , component: RegisterComponent}
+  {path : 'register' , component: RegisterComponent},
+  {path : 'admin' , component: AdminComponent}
 ];
---
+++ frontend/src/app/pages/adminDashboard/admin/admin.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {SidebareComponent} from '../sidebare/sidebare.component';
+import {LogementComponent} from '../logement/logement.component';
+
+@Component({
+  selector: 'app-admin',
+  imports: [SidebareComponent , LogementComponent],
+  templateUrl: './admin.component.html',
+  standalone: true,
+  styleUrl: './admin.component.css'
+})
+export class AdminComponent {
+
+}
